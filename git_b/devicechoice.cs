d53a49a35b99c3c3194e05da548b9da8e9376917
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2018-06-13 10:47:21 +0200

    keuze koffie of licht aan
    keuze koffie of licht aan

---
+++ git_b/devicechoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace b_opdracht
+{
+    class devicechoice
+    {
+        public bool koffieAan = false;
+        public bool lichtAan = false;
+    }
+}
