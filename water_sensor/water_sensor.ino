0688a666eaff50531c372a7b3449e7695b5fabdb
Janvdtuin1 <32636731+Janvdtuin1@users.noreply.github.com>
2018-06-19 11:23:25 +0200

    Water sensor sketch, basis.

---
+++ water_sensor/water_sensor.ino
@@ -0,0 +1,33 @@
+/* Print values from analog pin A0 to serial monitor */
+
+
+void setup()
+{
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  int sensorvalue = analogRead(A0);
+  Serial.println(sensorvalue);
+
+  
+  delay(150);
+
+  if(sensorvalue <300){
+
+
+    Serial.println("Bijvullen");
+    delay(1000);
+  }
+
+  if(sensorvalue >300){
+
+    Serial.println("Genoeg water");
+    delay(1000);
+  }
+
+ 
+}
+
