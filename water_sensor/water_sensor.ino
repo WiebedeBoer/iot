44b8cab6c19788042701b831db5e4eba862f8c09
Janvdtuin1 <32636731+Janvdtuin1@users.noreply.github.com>
2018-06-20 14:02:05 +0200

    Add files via upload

--- water_sensor/water_sensor.ino
+++ water_sensor/water_sensor.ino
@@ -1,32 +1,75 @@
-/* Print values from analog pin A0 to serial monitor */
+#include <SPI.h>
+#include <Ethernet.h>
 
+
+byte mac[] = {0x40, 0x6c, 0x8f, 0x36, 0x84, 0x8a}; //mac adres van het ethernetshield
+IPAddress ip(192,168,1,3);
+EthernetServer server(3300);
+bool connected = false;
+
+int WaterSensor = 0; //Watersensor variable
 
 void setup()
 {
   Serial.begin(9600);
-  
+  Ethernet.begin(mac, ip);
+    Serial.print("Listening on adres: "); Serial.println(Ethernet.localIP());
+
+server.begin();
+  delay(100);
 }
 
 void loop()
 {
-  int sensorvalue = analogRead(A0);
-  Serial.println(sensorvalue);
+    EthernetClient client = server.available();
+    
+    if(!client) return;
+      Serial.println("Niet verbonden");
+
+
+  while(client.connected())
+    {
+
+//watersensor lezen
+  WaterSensor = analogRead(A0);
+  Serial.println(WaterSensor);
+
+client.print(WaterSensor);
+      delay(150);
+
+
+  if(WaterSensor <300){
+
+
+    client.println("Bijvullen");
+    delay(1000);
+  }
+
+  if(WaterSensor >300){
+
+    client.println("Genoeg water");
+    delay(1000);
+  }
+
+
+
+    case 'w': //sensor waardes van watersensor naar app
+            intToCharBuf(WaterSensor, buf, 4);                // convert to charbuffer
+            if (bufferGlobal != 0)
+            {
+              sensorReadInterval = 1000 * bufferGlobal.toInt();
+            }
+            server.write(buf, 4);                             // response is always 4 chars (\n included)
+            Serial.print("Sensor: Case A "); Serial.println(buf);
+
+
+    }
 
   
-  delay(150);
-
-  if(sensorvalue <300){
 
 
-    Serial.println("Bijvullen");
-    delay(1000);
-  }
-
-  if(sensorvalue >300){
-
-    Serial.println("Genoeg water");
-    delay(1000);
-  }
+  
+  
 
  
 }
